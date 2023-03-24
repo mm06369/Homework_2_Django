3090b14a5a996f23a5192a9e32bb081e753ff860
mm06369 <77888296+mm06369@users.noreply.github.com>
2023-03-24 23:32:44 +0500

    added sign up screen

--- portfolio_website/portfolioWebsite/urls.py
+++ portfolio_website/portfolioWebsite/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name = 'home'),
-    path('signup', views.signup, name = 'signup'),
-    path('signin', views.signin, name = 'signin'),
+    path('signUp', views.signUp, name = 'signUp'),
+    path('signIn', views.signIn, name = 'signIn'),
     path('signout', views.signout, name = 'signout'),  
 ]
--- portfolio_website/portfolioWebsite/views.py
+++ portfolio_website/portfolioWebsite/views.py
@@ -1,16 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     return render(request, 'authentication/index.html')
 
-def signup(request):
-    return render(request,'portfolioWebsite/signup.html')
+def signUp(request):
 
-def signin(request):
-    return render(request,'portfolioWebsite/signin.html')
+    if request.method == 'POST':
+        name = request.POST['fname']
+        usern = request.POST['usern']
+        password = request.POST['password']
+        email = request.POST['email']
+        if email == None:
+            email = 'NULL'
+        if password == None:
+            password = 'NULL'
+        if usern == None:
+            usern = 'NULL'
+        if name == None:
+            name = 'NULL'
+
+        myUser = User.objects.create_user(usern, email, password)
+        myUser.first_name = name
+        myUser.last_name = name
+        myUser.save()
+
+        messages.success(request, 'Your account has been created')
+
+        return redirect('signIn')
+        
+
+    return render(request,'authentication/signUp.html')
+
+def signIn(request):
+    return render(request,'authentication/signIn.html')
 
 def signout(request):
     pass
